5b82fc6e7904b80c4c5dfad38e7604e61d482bcc
Ana Rosario <arosario.mrd@gmail.com>
2022-11-13 20:42:42 -0500

    commit tree 2ca3146ef0d1eb702dc6bfbddbdc63113717572a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const element = document.createElement("div");
+document.body.append(element);
+const ul = document.createElement("ul");
+for (let i=0; i<3;i++) {
+    const li = document.createElement("li");
+    li.textContent = (i+1).toString();
+    ul.append(li);
+}
+element.append(ul);
+
+const elemen = documeent.getElementByID("main");
+element.style.height ="300px";
+element.style.backgroundColor = "#27647B";
